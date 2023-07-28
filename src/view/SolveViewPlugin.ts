357024b588b13f17b245f116f9618cfba74a70e0
Liam Riddell <web@liamriddell.co.uk>
2023-07-28 13:27:09 +0100

    Prettier Fixes

--- src/view/SolveViewPlugin.ts
+++ src/view/SolveViewPlugin.ts
@@ -1,77 +1,88 @@
-// @ts-expect-error
+//// @ts-expect-error
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { syntaxTree } from "@codemirror/language";
-import { ViewUpdate, PluginValue, EditorView, ViewPlugin, PluginSpec, Decoration, DecorationSet } from "@codemirror/view";
-import { RangeSetBuilder } from "@codemirror/state";
+// import { syntaxTree } from "@codemirror/language";
 import { SolveResultWidget } from "@/widgets/SolveResultWidget";
+import { RangeSetBuilder } from "@codemirror/state";
+import {
+	Decoration,
+	DecorationSet,
+	EditorView,
+	PluginSpec,
+	PluginValue,
+	ViewPlugin,
+	ViewUpdate,
+} from "@codemirror/view";
 
 class SolveViewPlugin implements PluginValue {
-    decorations: DecorationSet;
+	decorations: DecorationSet;
 
-    constructor(view: EditorView) {
-        console.debug(`[SolveViewPlugin] Constructer`);
-        this.decorations = this.buildDecorations(view);
-    }
+	constructor(view: EditorView) {
+		console.debug(`[SolveViewPlugin] Constructer`);
+		this.decorations = this.buildDecorations(view);
+	}
 
-    update(update: ViewUpdate) {
-        if (update.docChanged || update.viewportChanged) {
-            // console.debug(`[SolveViewPlugin] Document/View Changed`, update.state.doc.toString());
-            this.decorations = this.buildDecorations(update.view);
-        }
-    }
+	update(update: ViewUpdate) {
+		if (update.docChanged || update.viewportChanged) {
+			// console.debug(`[SolveViewPlugin] Document/View Changed`, update.state.doc.toString());
+			this.decorations = this.buildDecorations(update.view);
+		}
+	}
 
-    destroy() {
-        console.debug(`[SolveViewPlugin] Destroyed`);
-    }
+	destroy() {
+		console.debug(`[SolveViewPlugin] Destroyed`);
+	}
 
-    buildDecorations(view: EditorView): DecorationSet {
-        const builder = new RangeSetBuilder<Decoration>();
+	buildDecorations(view: EditorView): DecorationSet {
+		const builder = new RangeSetBuilder<Decoration>();
 
-        for (const { from, to } of view.visibleRanges) {
-            console.debug(from, to);
+		for (const { from, to } of view.visibleRanges) {
+			console.debug(from, to);
 
-            builder.add(
-                from,
-                from,
-                Decoration.replace({
-                    widget: new SolveResultWidget("2001")
-                })
-            );
-        }
+			builder.add(
+				from,
+				from,
+				Decoration.replace({
+					widget: new SolveResultWidget("2001"),
+				})
+			);
+		}
 
-        return builder.finish();
-    }
+		return builder.finish();
+	}
 
-    // buildDecorations(view: EditorView): DecorationSet {
-    // 	const builder = new RangeSetBuilder<Decoration>();
+	// buildDecorations(view: EditorView): DecorationSet {
+	// 	const builder = new RangeSetBuilder<Decoration>();
 
-    // 	for (const { from, to } of view.visibleRanges) {
-    // 		syntaxTree(view.state).iterate({
-    // 			from,
-    // 			to,
-    // 			enter(node: any) {
-    // 				if (node.type.name.startsWith("list")) {
-    // 					// Position of the '-' or the '*'.
-    // 					const listCharFrom = node.from - 2;
+	// 	for (const { from, to } of view.visibleRanges) {
+	// 		syntaxTree(view.state).iterate({
+	// 			from,
+	// 			to,
+	// 			enter(node: any) {
+	// 				if (node.type.name.startsWith("list")) {
+	// 					// Position of the '-' or the '*'.
+	// 					const listCharFrom = node.from - 2;
 
-    // 					builder.add(
-    // 						listCharFrom,
-    // 						listCharFrom + 1,
-    // 						Decoration.widget({
-    // 							widget: new EmojiWidget(),
-    // 						})
-    // 					);
-    // 				}
-    // 			},
-    // 		});
-    // 	}
+	// 					builder.add(
+	// 						listCharFrom,
+	// 						listCharFrom + 1,
+	// 						Decoration.widget({
+	// 							widget: new EmojiWidget(),
+	// 						})
+	// 					);
+	// 				}
+	// 			},
+	// 		});
+	// 	}
 
-    // 	return builder.finish();
-    // }
+	// 	return builder.finish();
+	// }
 }
 
 const pluginSpec: PluginSpec<SolveViewPlugin> = {
-    decorations: (value: SolveViewPlugin) => value.decorations
+	decorations: (value: SolveViewPlugin) => value.decorations,
 };
 
-export const solveViewPlugin = ViewPlugin.fromClass(SolveViewPlugin, pluginSpec);
+export const solveViewPlugin = ViewPlugin.fromClass(
+	SolveViewPlugin,
+	pluginSpec
+);
--- src/widgets/SolveResultWidget.ts
+++ src/widgets/SolveResultWidget.ts
@@ -1,28 +1,28 @@
 import { EditorView, WidgetType } from "@codemirror/view";
 
 export class SolveResultWidget extends WidgetType {
-    value: string;
+	value: string;
 
-    constructor(value: string) {
-        super();
-        this.value = value;
-    }
+	constructor(value: string) {
+		super();
+		this.value = value;
+	}
 
-    toDOM(view: EditorView): HTMLElement {
-        const div = document.createElement("div");
+	toDOM(view: EditorView): HTMLElement {
+		const div = document.createElement("div");
 
-        div.textContent = `= ${this.value}`;
+		div.textContent = `= ${this.value}`;
 
-        div.setCssStyles({
-            height: `${view.defaultLineHeight}px`,
-            display: 'inline-flex',
-            background: 'rgba(34, 206, 153, 0.2)',
-            color: '22CE99',
-            padding: '0 4px',
-            borderRadius: '4px',
-            marginRight: '4px'
-        });
+		div.setCssStyles({
+			height: `${view.defaultLineHeight}px`,
+			display: "inline-flex",
+			background: "rgba(34, 206, 153, 0.2)",
+			color: "22CE99",
+			padding: "0 4px",
+			borderRadius: "4px",
+			marginRight: "4px",
+		});
 
-        return div;
-    }
+		return div;
+	}
 }
