199850be4ce75694f7a5fed79c141c1902a5cb6a
Liam Riddell <web@liamriddell.co.uk>
2023-09-17 15:45:52 +0100

    Consistency refactor

--- test/providers/datetime/DatetimeProvider.test.ts
+++ test/providers/datetime/DatetimeProvider.test.ts
@@ -4,15 +4,15 @@ import { StringResult } from "@/results/StringResult";
 import { beforeAll, describe, expect, test } from "@jest/globals";
 import moment from "moment";
 
-let datetimeProvider: DatetimeProvider;
+let provider: DatetimeProvider;
 
 beforeAll(() => {
-	datetimeProvider = new DatetimeProvider();
+	provider = new DatetimeProvider();
 });
 
 describe("Primitive", () => {
 	test("Now", () => {
-		const result = datetimeProvider.provide("Now");
+		const result = provider.provide("Now");
 
 		expect(result).toBeDefined();
 
@@ -20,7 +20,7 @@ describe("Primitive", () => {
 	});
 
 	test("Today", () => {
-		const result = datetimeProvider.provide("Today");
+		const result = provider.provide("Today");
 
 		expect(result).toBeDefined();
 
@@ -28,7 +28,7 @@ describe("Primitive", () => {
 	});
 
 	test("Tomorrow", () => {
-		const result = datetimeProvider.provide("Tomorrow");
+		const result = provider.provide("Tomorrow");
 
 		expect(result).toBeDefined();
 
@@ -36,7 +36,7 @@ describe("Primitive", () => {
 	});
 
 	test("Yesterday", () => {
-		const result = datetimeProvider.provide("Yesterday");
+		const result = provider.provide("Yesterday");
 
 		expect(result).toBeDefined();
 
@@ -57,7 +57,7 @@ describe("Primitive", () => {
 		for (let i = 0; i < days.length; i++) {
 			const day = days[i];
 
-			const result = datetimeProvider.provide(`Next ${day}`);
+			const result = provider.provide(`Next ${day}`);
 			expect(result).toBeDefined();
 			expect(result).toBeInstanceOf(DatetimeResult);
 		}
@@ -77,7 +77,7 @@ describe("Primitive", () => {
 		for (let i = 0; i < days.length; i++) {
 			const day = days[i];
 
-			const result = datetimeProvider.provide(`Last ${day}`);
+			const result = provider.provide(`Last ${day}`);
 			expect(result).toBeDefined();
 			expect(result).toBeInstanceOf(DatetimeResult);
 		}
@@ -86,55 +86,55 @@ describe("Primitive", () => {
 
 describe("Date", () => {
 	test("1/1/23", () => {
-		const result = datetimeProvider.provide("1/1/23");
+		const result = provider.provide("1/1/23");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1/1/2023", () => {
-		const result = datetimeProvider.provide("1/1/2023");
+		const result = provider.provide("1/1/2023");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("01/01/2023", () => {
-		const result = datetimeProvider.provide("01/01/2023");
+		const result = provider.provide("01/01/2023");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1-1-23", () => {
-		const result = datetimeProvider.provide("1-1-23");
+		const result = provider.provide("1-1-23");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1-1-2023", () => {
-		const result = datetimeProvider.provide("1-1-2023");
+		const result = provider.provide("1-1-2023");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("01-01-2023", () => {
-		const result = datetimeProvider.provide("01-01-2023");
+		const result = provider.provide("01-01-2023");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1.1.23", () => {
-		const result = datetimeProvider.provide("1.1.23");
+		const result = provider.provide("1.1.23");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1.1.2023", () => {
-		const result = datetimeProvider.provide("1.1.2023");
+		const result = provider.provide("1.1.2023");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("01.01.2023", () => {
-		const result = datetimeProvider.provide("01.01.2023");
+		const result = provider.provide("01.01.2023");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
@@ -142,37 +142,37 @@ describe("Date", () => {
 
 describe("ISO8601", () => {
 	test("1997-07-16", () => {
-		const result = datetimeProvider.provide("1997-07-16");
+		const result = provider.provide("1997-07-16");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1997-07-16T19:20:30", () => {
-		const result = datetimeProvider.provide("1997-07-16T19:20:30");
+		const result = provider.provide("1997-07-16T19:20:30");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1997-07-16T19:20:30.45Z", () => {
-		const result = datetimeProvider.provide("1997-07-16T19:20:30.45Z");
+		const result = provider.provide("1997-07-16T19:20:30.45Z");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1997-07-16T19:20:30.45", () => {
-		const result = datetimeProvider.provide("1997-07-16T19:20:30.45");
+		const result = provider.provide("1997-07-16T19:20:30.45");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1997-07-16T19:20:30.45+01:00", () => {
-		const result = datetimeProvider.provide("1997-07-16T19:20:30.45+01:00");
+		const result = provider.provide("1997-07-16T19:20:30.45+01:00");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
 
 	test("1997-07-16T19:20:30.45-01:00", () => {
-		const result = datetimeProvider.provide("1997-07-16T19:20:30.45-01:00");
+		const result = provider.provide("1997-07-16T19:20:30.45-01:00");
 		expect(result).toBeDefined();
 		expect(result).toBeInstanceOf(DatetimeResult);
 	});
@@ -182,9 +182,7 @@ describe("Addition & Subtraction", () => {
 	test("Now + 2 days", () => {
 		const nowTwoDaysAhead = moment().add(2, "days");
 
-		const result = datetimeProvider.provide(
-			"Now + 2 days"
-		) as DatetimeResult;
+		const result = provider.provide("Now + 2 days") as DatetimeResult;
 
 		expect(result).toBeDefined();
 
@@ -200,9 +198,7 @@ describe("Addition & Subtraction", () => {
 	test("Now - 2 days", () => {
 		const nowTwoDaysAgo = moment().subtract(2, "days");
 
-		const result = datetimeProvider.provide(
-			"Now - 2 days"
-		) as DatetimeResult;
+		const result = provider.provide("Now - 2 days") as DatetimeResult;
 
 		expect(result).toBeDefined();
 
@@ -222,7 +218,7 @@ describe("Functions", () => {
 
 		const prompt = `years until ${inFuture.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
@@ -236,7 +232,7 @@ describe("Functions", () => {
 
 		const prompt = `months until ${inFuture.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
@@ -250,7 +246,7 @@ describe("Functions", () => {
 
 		const prompt = `weeks until ${inFuture.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
@@ -264,7 +260,7 @@ describe("Functions", () => {
 
 		const prompt = `days until ${inFuture.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
@@ -278,7 +274,7 @@ describe("Functions", () => {
 
 		const prompt = `years since ${inFuture.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
@@ -292,7 +288,7 @@ describe("Functions", () => {
 
 		const prompt = `months since ${inFuture.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
@@ -306,7 +302,7 @@ describe("Functions", () => {
 
 		const prompt = `weeks since ${inFuture.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
@@ -320,7 +316,7 @@ describe("Functions", () => {
 
 		const prompt = `days since ${inPast.format("DD/MM/YYYY")}`;
 
-		const result = datetimeProvider.provide(prompt) as StringResult;
+		const result = provider.provide(prompt) as StringResult;
 
 		expect(result).toBeDefined();
 
