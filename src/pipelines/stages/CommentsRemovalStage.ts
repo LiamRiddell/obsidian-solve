0253518a50ca9aa339daf90b2b4ad1657d7aa7fe
Liam Riddell <web@liamriddell.co.uk>
2023-11-27 12:32:36 +0000

    Fixed comment

--- src/pipelines/stages/CommentsRemovalStage.ts
+++ src/pipelines/stages/CommentsRemovalStage.ts
@@ -1,7 +1,7 @@
 import { BasePipelineStage } from "@/pipelines/definition/stages/BasePipelineStage";
 
 export class CommentsRemovalStage extends BasePipelineStage<string> {
-	// Matches for either # or // followed by some any text.
+	// Matches for either # or // followed by any text.
 	private commentsRemovalRegex = new RegExp(/((?:#|\/\/).+)/);
 
 	protected execute(request: string): string {
