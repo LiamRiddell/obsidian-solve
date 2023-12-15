1df2add66cfbf64a7931ad64239c3332ea00346e
Liam Riddell <web@liamriddell.co.uk>
2023-12-15 20:13:42 +0000

    Fixed tests for variable change

--- test/pipeline/stages/PreviousResultSubstitutionStage.spec.ts
+++ test/pipeline/stages/PreviousResultSubstitutionStage.spec.ts
@@ -12,7 +12,7 @@ describe("Previous Result Substitution", () => {
 	test("Single Previous Substitutation", () => {
 		stage.setPreviousResult(new NumberResult(100));
 
-		const substituted = stage.process("12 + $prev");
+		const substituted = stage.process("12 + :prev");
 
 		expect(substituted).toBeDefined();
 
@@ -22,7 +22,7 @@ describe("Previous Result Substitution", () => {
 	test("Multiple Previous Substitutation", () => {
 		stage.setPreviousResult(new NumberResult(34));
 
-		const substituted = stage.process("$prev + 12 + $prev");
+		const substituted = stage.process(":prev + 12 + :prev");
 
 		expect(substituted).toBeDefined();
 
@@ -31,17 +31,17 @@ describe("Previous Result Substitution", () => {
 
 	test("Multiple Line Previous Substitutations", () => {
 		stage.setPreviousResult(new NumberResult(34));
-		const line1 = stage.process("$prev + 12");
+		const line1 = stage.process(":prev + 12");
 		expect(line1).toBeDefined();
 		expect(line1).toEqual("34 + 12");
 
 		stage.setPreviousResult(new NumberResult(24));
-		const line2 = stage.process("$prev + 12");
+		const line2 = stage.process(":prev + 12");
 		expect(line2).toBeDefined();
 		expect(line2).toEqual("24 + 12");
 
 		stage.setPreviousResult(new NumberResult(12));
-		const line3 = stage.process("$prev + 12");
+		const line3 = stage.process(":prev + 12");
 		expect(line3).toBeDefined();
 		expect(line3).toEqual("12 + 12");
 	});
